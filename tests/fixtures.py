33448511c3106c1f385ee3fe3062bd5331fe3224
Kiran Kumari <kiran1999rp@gmail.com>
2022-01-19 20:00:48 +0530

    created fixtures

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,8 +1,8 @@
 from rest_framework.test import APIClient
 import pytest
 
-from core.models import MealItem, User
-from .factories import UserFactory, MealItemFactory
+from core.models import MealItem, User, Meal, Menu, Mess
+from .factories import MealFactory, UserFactory, MealItemFactory, MessFactory, MenuFactory
 
 
 @pytest.fixture(scope="function")
@@ -25,4 +25,22 @@ def create_meal_item():
 
     return _create_meal_item
 
-# @pytest.fixture(scope="function")
+
+@pytest.fixture(scope="function")
+def create_meal():
+    def _create_meal(**kwargs) -> Meal:
+        return MealFactory.create(**kwargs)
+
+    return _create_meal
+
+
+@pytest.fixture(scope="function")
+def create_mess():
+    def _create_mess(**kwargs) -> Mess:
+        return MessFactory.create(**kwargs)
+
+
+@pytest.fixture(scope="function")
+def create_menu():
+    def _create_menu(**kwargs) -> Menu:
+        return MenuFactory.create(**kwargs)
