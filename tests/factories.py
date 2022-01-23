78169dfb6fdeaecac064d12784167254b62fd93c
Kiran Kumari <kiran1999rp@gmail.com>
2022-01-23 10:46:02 +0530

    Added superuserfactory
    Co-authored-by: Shailesh Aanand <anaandshailu@gmail.com>

--- tests/factories.py
+++ tests/factories.py
@@ -7,12 +7,33 @@ from core.models import Meal, MealItem, Menu, Mess, User as UserModel
 User: UserModel = get_user_model()
 
 MEAL_ITEMS = [
-    "roti", "daal", "chawal", "puri", "paratha", "idli", "sambhar", "vada",
-    "chutney", "papad", "salad", "mix veg", "chola", "bhatura", "paneer", "chicken"
+    "roti",
+    "daal",
+    "chawal",
+    "puri",
+    "paratha",
+    "idli",
+    "sambhar",
+    "vada",
+    "chutney",
+    "papad",
+    "salad",
+    "mix veg",
+    "chola",
+    "bhatura",
+    "paneer",
+    "chicken",
 ]
 
-MEALS = ["Puri Sabji", "Idli", "Chicken", "Kofta",
-         "Rajma Chawal", "Chicken Chiili", "Mix Veg"]
+MEALS = [
+    "Puri Sabji",
+    "Idli",
+    "Chicken",
+    "Kofta",
+    "Rajma Chawal",
+    "Chicken Chiili",
+    "Mix Veg",
+]
 
 MESS_NAMES = ["hostel no.20", "hostel no.21"]
 
@@ -36,8 +57,16 @@ class UserFactory(factory.django.DjangoModelFactory):
         return cls._get_manager(model_class).create_user(*args, **kwargs)
 
 
-class MessFactory(factory.django.DjangoModelFactory):
+class SuperUserFactory(UserFactory):
+    is_superuser = True
+    is_staff = True
 
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        return cls._get_manager(model_class).create_superuser(*args, **kwargs)
+
+
+class MessFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Mess
 
@@ -46,7 +75,6 @@ class MessFactory(factory.django.DjangoModelFactory):
 
 
 class MealItemFactory(factory.django.DjangoModelFactory):
-
     class Meta:
         model = MealItem
 
@@ -59,8 +87,9 @@ class MealFactory(factory.django.DjangoModelFactory):
 
     name = factory.LazyFunction(lambda: random.choice(MEALS))
     current_price = factory.Faker("random_int", min=40, max=100, step=5)
-    type = factory.LazyFunction(lambda: random.choice(
-        [x.value for x in Meal.MEAL_TYPE]))
+    type = factory.LazyFunction(
+        lambda: random.choice([x.value for x in Meal.MEAL_TYPE])
+    )
 
     @factory.post_generation
     def meal_items(self, create, extracted, **kwargs):
@@ -76,7 +105,6 @@ class MealFactory(factory.django.DjangoModelFactory):
 
 
 class MenuFactory(factory.django.DjangoModelFactory):
-
     class Meta:
         model = Menu
 
